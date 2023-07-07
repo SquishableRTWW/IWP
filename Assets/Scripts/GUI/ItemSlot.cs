16ab8b5b21a3c581d2a25dd474a8907cc0bdd9c7
SquishableRTWW <SquishableRTWW@gmail.com>
2023-07-07 14:41:15 +0800

    swapped the rest of the weapons to fit drag drop logic

--- Assets/Scripts/GUI/ItemSlot.cs
+++ Assets/Scripts/GUI/ItemSlot.cs
@@ -5,14 +5,21 @@ using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour, IDropHandler
 {
+    public string slotType;
+    public GameObject itemInside;
+
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("Dropped in");
         // NOTE: Pointer Drag refers to the gameObject
         if (eventData.pointerDrag != null)
         {
+            // Snap the item's position
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            // Set its slot boolean to true
             eventData.pointerDrag.GetComponent<DragDrop>().isInSlot = true;
+            // Set the itemslot's item to this gameobject.
+            itemInside = eventData.pointerDrag;
         }
     }
 }
