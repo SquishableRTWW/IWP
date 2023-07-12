04b6a19bbf8dbbae51b1cf4ea83c904097227984
SquishableRTWW <SquishableRTWW@gmail.com>
2023-07-12 17:44:54 +0800

    Created blank event canvas

--- Assets/Scripts/ManagerController/EventManager.cs
+++ Assets/Scripts/ManagerController/EventManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
+    public int randomEventNumber;
+    public string eventType;
+
     // Start is called before the first frame update
     void Start()
     {
